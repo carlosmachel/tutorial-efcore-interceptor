97b0acd9c6f7c2347d8ceae8395b36dbe93e9ac5
Carlos Machel <carlostiago@outlook.com.br>
2023-01-11 17:54:14 -0300

    Interceptor

---
+++ EFCoreInterceptorsDemo.Data.EF/Student.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Data.Common;
+
+namespace EFCoreInterceptorsDemo.Data.EF
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // ALERT - DEMO CODE
+            // You may want to inject the interceptors into the context 
+            optionsBuilder.AddInterceptors(new DemoDbCommandInterceptor());
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+    public class DemoDbCommandInterceptor : DbCommandInterceptor
+    {
+        public override InterceptionResult<DbDataReader> ReaderExecuting(
+            DbCommand command,
+            CommandEventData eventData,
+            InterceptionResult<DbDataReader> result)
+        {
+            ModifyCommand(command);
+
+            return result;
+        }
+
+        public override ValueTask<InterceptionResult<DbDataReader>> ReaderExecutingAsync(
+            DbCommand command,
+            CommandEventData eventData,
+            InterceptionResult<DbDataReader> result,
+            CancellationToken cancellationToken = default)
+        {
+            ModifyCommand(command);
+
+            return new ValueTask<InterceptionResult<DbDataReader>>(result);
+        }
+
+        private static void ModifyCommand(DbCommand command)
+        {
+            if (!command.CommandText.ToLower().Contains("where", StringComparison.Ordinal))
+            {
+                Console.WriteLine("DemoDbCommandInterceptor: Applying Where");
+                command.CommandText += " Where Id = 1";
+            }
+            else
+            {
+                Console.WriteLine("DemoDbCommandInterceptor: Applying Where");
+                command.CommandText += " And Id = 2";
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public DateTime DeletedOn { get; set; }
+
+        public override string ToString()
+        => $"{{ Id: {Id}, FirstName={FirstName}, LastName:{LastName} }}";
+    }
+}
---
+++ EFCoreInterceptorsDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using EFCoreInterceptorsDemo.Data.EF;
+using Microsoft.EntityFrameworkCore;
+
+//AddStudents();
+
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine("With Interceptor....");
+await GetAllStudents(applyInterceptor: true);
+Console.WriteLine("---------------------------------------------");
+
+Console.WriteLine("Without Interceptor....");
+await GetAllStudents(applyInterceptor: false);
+Console.WriteLine("---------------------------------------------");
+
+void AddStudents()
+{
+    using var context = BuildUniversityContext();
+    context.Add(
+        new Student
+        {
+            Id =  1,
+            FirstName = "John",
+            LastName = "Doe",
+            Address = "4 Privet Drive",
+        });
+
+    context.Add(
+        new Student
+        {
+            Id = 2,
+            FirstName = "Jane",
+            LastName = "Doe",
+            Address = "4 Privet Drive",
+        });
+    context.SaveChanges();
+}
+
+async Task GetAllStudents(bool applyInterceptor)
+{
+    using var context = BuildUniversityContext();
+
+    IList<Student> studentCollection = null;
+    if (applyInterceptor)
+    {
+        studentCollection = await context.Students.Where(s => s.LastName != "").ToListAsync();
+    }
+    else
+    {
+        studentCollection = await context.Students.ToListAsync();
+    }
+
+    Console.WriteLine("Printing List of Students");
+    foreach (var student in studentCollection)
+    {
+        Console.WriteLine(student);
+    }
+}
+
+UniversityContext BuildUniversityContext()
+{
+    var dbContextBuilder = new DbContextOptionsBuilder<UniversityContext>();
+
+    // HARDCODED - For this demo.  
+    var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=University; Integrated Security=True;";
+
+    dbContextBuilder.UseSqlServer(connectionString);
+    return new UniversityContext(dbContextBuilder.Options);
+}
